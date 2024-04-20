4281f2c158111b77eab256933d3ee3776f1de671
Roshni <162877854+rbha917@users.noreply.github.com>
2024-04-20 21:33:48 +1200

    formative feedback changes

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-let scoopY = 350; 
-let iceCreamY = 350; 
+let scoopY = 600; 
+let iceCreamY = 600; 
 
 function setup() {
-  createCanvas(400, 400);
-  background(220);
+  createCanvas(windowWidth, windowHeight);
+  background(50);
 }
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+	}
 
 function icecream(x, y, size, colour){
   noStroke();
@@ -18,17 +22,17 @@ function icecream(x, y, size, colour){
 countInterval = setInterval(addScoop, 1000);
 
 function addScoop() {
-  icecream(20, iceCreamY, 30, color(255));
+  icecream(width/2, iceCreamY, 30, color(255));
   scoopY -= 5;
   let r = random(255);
   let g = random(255);
   let b = random(255);
   fill(r, g, b);
   ellipseMode(CORNER);
-  ellipse(20, scoopY - 30, 40, 41);
+  ellipse(width/2, scoopY - 30, 40, 41);
   if (scoopY < 40) {
-    background(220);
-    icecream(20, iceCreamY, 30, color(255));
+    background(50);
+    icecream(width/2, iceCreamY, 30, color(255));
     scoopY = iceCreamY - 5;
   }
 }
